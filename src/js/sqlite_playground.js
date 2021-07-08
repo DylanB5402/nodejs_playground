29074dda22e63ee2ed71c45b71c8d30a1e7658b7
Dylan Barva <d.barva5402@gmail.com>
2021-07-08 14:49:00 -0700

    add basic database code

---
+++ src/js/sqlite_playground.js
@@ -0,0 +1,17 @@
+var sqlite3 = require('sqlite3').verbose();
+
+var db = new sqlite3.Database('test.db');
+
+db.serialize( () => {
+    db.run("INSERT INTO students (name, num) VALUES (\"Cheese\", 19)")
+})
+
+db.serialize( () => {
+    db.all("SELECT * FROM students", (err, rows) => {
+        // console.log(rows);
+        for (i = 0; i < rows.length; i++) {
+            console.log(rows[i])
+        }
+    })
+})
+db.close()
