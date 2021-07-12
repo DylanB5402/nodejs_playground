de7b48db8edbed72c790de0e4ac891f781b58b8a
Dylan Barva <d.barva5402@gmail.com>
2021-07-12 13:22:36 -0700

    more work on server/database

--- src/js/server_express.js
+++ src/js/server_express.js
@@ -21,8 +21,13 @@ app.post('/', (req, res) => {
         resp = "potato"
     }
     res.send(resp)
+    res.sendF
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
 })
--- src/js/sqlite_playground.js
+++ src/js/sqlite_playground.js
@@ -2,15 +2,15 @@ var sqlite3 = require('sqlite3').verbose();
 
 var db = new sqlite3.Database('test.db');
 
-db.serialize( () => {
-    db.run("INSERT INTO students (name, num) VALUES (\"Cheese\", 19)")
-})
+// db.serialize( () => {
+//     db.run("INSERT INTO students (name, num) VALUES (\"Cheese\", 19)")
+// })
 
 db.serialize( () => {
-    db.all("SELECT * FROM students", (err, rows) => {
-        // console.log(rows);
+    db.all("SELECT name FROM students", (err, rows) => {
+        console.log(rows);
         for (i = 0; i < rows.length; i++) {
-            console.log(rows[i])
+            console.log(rows[i]['name'])
         }
     })
 })
