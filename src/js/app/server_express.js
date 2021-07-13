71bc340efca9397de1aee82d4d6a0e24df8b7885
Dylan Barva <d.barva5402@gmail.com>
2021-07-13 13:30:17 -0700

    return data from sql in a request

--- src/js/app/server_express.js
+++ src/js/app/server_express.js
@@ -1,17 +1,24 @@
-import express, { json, urlencoded } from 'express';
+// import express, { json, urlencoded } from 'express';
+// import {SQLiteDatabase } from './sqlite_callbacks'
+
+const express = require('express')
+const sqlite_callbacks = require('./sqlite_callbacks')
 
 const app = express()
 const port = 3000
 
-app.use(json()) // for parsing application/json
-app.use(urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+var database = new sqlite_callbacks.SQLiteDatabase('test.db')
 
 app.get('/', (req, res) => {
   res.send('GET request received!')
+  
 })
 
 app.get('/students', (req, res) => {
-  res.send('GET request received!')
+  // res.send('GET request received!')
+  database.getAllStudentNames(res);
 })
 
 app.post('/', (req, res) => {
